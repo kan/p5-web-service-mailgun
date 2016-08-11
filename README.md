f49898d09486edc97a8f9846c1ce82e37c989f52
Kan Fushihara <kan.fushihara@gmail.com>
2016-08-12 08:58:01 +0900

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WebService::Mailgun - It's new $module
+
+# SYNOPSIS
+
+    use WebService::Mailgun;
+
+# DESCRIPTION
+
+WebService::Mailgun is ...
+
+# LICENSE
+
+Copyright (C) Kan Fushihara.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kan Fushihara <kan.fushihara@gmail.com>
---
+++ lib/WebService/Mailgun.pm
@@ -0,0 +1,39 @@
+package WebService::Mailgun;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::Mailgun - It's new $module
+
+=head1 SYNOPSIS
+
+    use WebService::Mailgun;
+
+=head1 DESCRIPTION
+
+WebService::Mailgun is ...
+
+=head1 LICENSE
+
+Copyright (C) Kan Fushihara.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kan Fushihara E<lt>kan.fushihara@gmail.comE<gt>
+
+=cut
+
